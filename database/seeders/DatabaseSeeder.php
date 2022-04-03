d10e7c85ba2dd6f35565d6e988d9ffae2f2e9c7f
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-04-04 00:45:12 +0100

    Created post content, added corresponding user and category IDs

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Category;
+use App\Models\Post;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,22 +20,44 @@ class DatabaseSeeder extends Seeder
 
         User::truncate();
         Category::truncate();
-        // \App\Models\User::factory(10)->create();
-        $user = User::factory(3)->create();
+        Post::truncate();
 
-        Category::create([
+        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create();
+
+       $personal = Category::create([
             'name' => 'PersonalCategory_name',
             'slug' => 'PersonalCategory_slug',
         ]);
 
-        Category::create([
+        $work = Category::create([
            'name' => 'WorkCategory_name',
            'slug' => 'WorkCategory_slug', 
         ]);
 
-        Category::create([
+        $family = Category::create([
             'name' => 'FamilyCategory_name',
             'slug' => 'FamilyCategory_slug',
+        ]);
+
+        //post tables
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $personal->id,
+            'title' => 'My personal Post under Personal Category',
+            'slug' => 'My_first_slug_personalPost',
+            'excerpt' => 'All about Personal excerpt',
+            'body' => 'Personal  is good in ones life',
+
+        ]);
+
+        Post::create([
+            'user_id' => $user->id,
+            'category_id' => $work->id,
+            'title' => 'My Work Post under work Category',
+            'slug' => 'My_first_slug_workPost',
+            'excerpt' => 'All about work excerpt',
+            'body' => 'Working  is good in ones life'
         ]);
     }
 }
