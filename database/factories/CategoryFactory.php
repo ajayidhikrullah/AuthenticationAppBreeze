e4957ae57722e7df6d15350ee930e7e49a1d0913
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-04-08 12:20:33 +0100

    Added Post Factory create method to create Category, post and User in DB

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace Database\Factories;
+use App\Models\User;
+// use App\Models\Category;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 
@@ -14,7 +16,14 @@ class CategoryFactory extends Factory
     public function definition()
     {
         return [
-            //
+            //category info
+            // 'user_id' => User::factory(),
+            // 'category_id' => Category::factory(),
+            'name' => $this->faker->word,
+            'slug' => $this->faker->slug
+            // 'excerpt' => $this->faker->sentence,
+            // 'body' => $this->faker->paragraph
+            
         ];
     }
 }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,42 +22,46 @@ class DatabaseSeeder extends Seeder
         Category::truncate();
         Post::truncate();
 
+        Post::factory(5)->create();
+
         // \App\Models\User::factory(10)->create();
-        $user = User::factory()->create();
+        // $user = User::factory()->create();
 
-       $personal = Category::create([
-            'name' => 'PersonalCategory_name',
-            'slug' => 'PersonalCategory_slug',
-        ]);
 
-        $work = Category::create([
-           'name' => 'WorkCategory_name',
-           'slug' => 'WorkCategory_slug', 
-        ]);
+    
+        //    $personal = Category::create([
+    //         'name' => 'PersonalCategory_name',
+    //         'slug' => 'PersonalCategory_slug',
+    //     ]);
 
-        $family = Category::create([
-            'name' => 'FamilyCategory_name',
-            'slug' => 'FamilyCategory_slug',
-        ]);
+    //     $work = Category::create([
+    //        'name' => 'WorkCategory_name',
+    //        'slug' => 'WorkCategory_slug', 
+    //     ]);
 
-        //post tables
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $personal->id,
-            'title' => 'My personal Post under Personal Category',
-            'slug' => 'My_first_slug_personalPost',
-            'excerpt' => 'All about Personal excerpt',
-            'body' => 'Personal  is good in ones life',
+    //     $family = Category::create([
+    //         'name' => 'FamilyCategory_name',
+    //         'slug' => 'FamilyCategory_slug',
+    //     ]);
 
-        ]);
+    //     //post tables
+    //     Post::create([
+    //         'user_id' => $user->id,
+    //         'category_id' => $personal->id,
+    //         'title' => 'My personal Post under Personal Category',
+    //         'slug' => 'My_first_slug_personalPost',
+    //         'excerpt' => 'All about Personal excerpt',
+    //         'body' => 'Personal  is good in ones life',
 
-        Post::create([
-            'user_id' => $user->id,
-            'category_id' => $work->id,
-            'title' => 'My Work Post under work Category',
-            'slug' => 'My_first_slug_workPost',
-            'excerpt' => 'All about work excerpt',
-            'body' => 'Working  is good in ones life'
-        ]);
+    //     ]);
+
+    //     Post::create([
+    //         'user_id' => $user->id,
+    //         'category_id' => $work->id,
+    //         'title' => 'My Work Post under work Category',
+    //         'slug' => 'My_first_slug_workPost',
+    //         'excerpt' => 'All about work excerpt',
+    //         'body' => 'Working  is good in ones life'
+    //     ]);
     }
 }
