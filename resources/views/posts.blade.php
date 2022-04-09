2666fc4fc0fa1045e3e190c17a4c63edf9e3a011
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-04-10 00:43:56 +0100

    added posts card in view, fetched the post title to show, created posts header

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,9 +1,12 @@
 <x-layout>
+    @include('_posts-header')
+
+
     
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card/>
+        <x-post-featured-card :post="$posts[0]"/>
 
-        <div class="lg:grid lg:grid-cols-2">
+        {{-- <div class="lg:grid lg:grid-cols-2">
             <x-post-card/>
             <x-post-card/>
         </div>
@@ -12,20 +15,9 @@
             <x-post-card/>
             <x-post-card/>
             <x-post-card/>          
-        </div>
+        </div> --}}
     </main>
-
-
-
-
-
-
-
-
-
-
-
-    
+   
     @foreach ($posts as $post)
     {{-- {{dd($posts)}} --}}
 
