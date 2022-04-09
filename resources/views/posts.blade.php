4e57061f742a6784c7b36cc142249f809b782db5
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-04-09 19:27:58 +0100

    Added post header, separated post articles using laravel blade layouts

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,4 +1,31 @@
 <x-layout>
+    
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card/>
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card/>
+            <x-post-card/>
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card/>
+            <x-post-card/>
+            <x-post-card/>          
+        </div>
+    </main>
+
+
+
+
+
+
+
+
+
+
+
+    
     @foreach ($posts as $post)
     {{-- {{dd($posts)}} --}}
 
@@ -8,7 +35,6 @@
                     {{$post->title}}
                 </a>
             </h1>
-
 
             {{-- fetch category names/properties--}}
             {{--
