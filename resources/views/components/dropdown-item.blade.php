3e2b123709667935d9cba856d3c2ef15849510d3
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-04-12 01:14:46 +0100

    adde dropdown arrow and item blade components

---
+++ resources/views/components/dropdown-item.blade.php
@@ -0,0 +1,9 @@
+@props(['active' => false])
+
+    @php
+        $classes = "block text-left px-3 leading-5 text-sm hover:bg-gray-300 focus:bg-gray-300";
+
+        if ($active) $classes .= 'bg-blue-500 text-gray';
+    @endphp
+
+<a {{$attributes(['class' => $classes]) }}>{{$slot}}</a>
