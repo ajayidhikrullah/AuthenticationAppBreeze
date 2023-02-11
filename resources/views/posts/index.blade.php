d5cd0a5f4d9802c705a734ce2d183672d230aa32
Ajayi Dhikrullah <ajayidhikrullah@gmail.com>
2023-02-11 23:21:57 +0000

    removed comments

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -6,6 +6,7 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if($posts->count())    
             <x-posts-grid :posts="$posts"/>
+            {{ $posts->links()}}
         @else
             <p class="text-center btn-warning">No Post YET o...</p>
         @endif    
@@ -18,7 +19,6 @@
     </main>
    
     @foreach ($posts as $post)
-    {{-- {{dd($posts)}} --}}
 
         <article>
             <h1>
