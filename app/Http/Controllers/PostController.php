77c763a4414fc671ad27747093e404f220e393ac
Ajayi Dhikrullah <ajayidhikrullah@gmail.com>
2023-03-10 06:06:10 +0000

    added pagination button, published tailwind vendor package and registered pagination in service provider

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -21,7 +21,7 @@ class PostController extends Controller
         return view('posts.index', [
             'posts' => Post::latest()->filter(
                 request(['search', 'category', 'author'])
-            )->paginate(3),
+            )->simplePaginate(3),
             'categories' => Category::all(),
         ]);
         // $posts = Post::latest();
