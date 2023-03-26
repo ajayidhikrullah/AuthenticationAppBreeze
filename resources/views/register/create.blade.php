6ece63caaa8a25df50268d44e77e9766a2cd14d4
Ajayi Dhikrullah <ajayidhikrullah@gmail.com>
2023-03-27 00:45:47 +0100

    added register controller, register route and view

---
+++ resources/views/register/create.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <section class="px-6 py-8">
+        <h1>Register Blade</h1>
+    </section>
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\RegisterController;
 use App\Models\Post;
 use App\Models\Category;
 use App\Models\User;
