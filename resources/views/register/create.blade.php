0d6746872a1db9699568c0070c3535fbbfe089b4
Ajayi Dhikrullah <ajayidhikrullah@gmail.com>
2023-04-13 06:52:33 +0100

    added register form

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -1,5 +1,16 @@
 <x-layout>
     <section class="px-6 py-8">
+        <main class="max-w-lg mx-auto mt-190">
+            <form action="/register" method="POST">
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
+                        Username
+                    </label>
+
+                    <input type="text" class="border border-gray-400 p-2 w-full" name="username" id="username" required>
+                </div>
+            </form>
+        </main>
         <h1>Register Blade</h1>
     </section>
 </x-layout>
